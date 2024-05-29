45945e1e3db762203e3a3420481110d054383648
j0a0qu3br4c0d1g0s <166523332+j0a0qu3br4c0d1g0s@users.noreply.github.com>
2024-05-29 08:46:58 -0300

    Delete verificador de elegibilidade para emprego.py

--- verificador de elegibilidade para emprego.py
+++
@@ -1,7 +0,0 @@
-idade = int(input("Digite a sua idade: "))
-nacionalidade = str(input("Digite a sua nacionalidade: "))
-experiência = int(input("Quantos anos de experiência você tem?: "))
-if idade >= 18 and nacionalidade == "brasileira" or "brasileiro" and experiência >= 2:
-   print("Você é elegível para a vaga de emprego.")
-else:
-   print("Desculpe, você não tem os requisistos para a vaga de emprego.")
